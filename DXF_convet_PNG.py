e719aea3defdc02a4845ce4f14b8411722542221
OscarAbad904 <sinnombre904@hotmail.com>
2023-12-13 19:57:25 +0100

    Versión 1.1

--- DXF_convet_PNG.py
+++ DXF_convet_PNG.py
@@ -1,7 +1,7 @@
 import sys
 import ezdxf
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle, Arc
+from matplotlib.patches import * 
 
 def DXF_convet_PNG():
     try:
@@ -9,8 +9,8 @@ def DXF_convet_PNG():
         ruta_dxf = sys.argv[1]
         ruta_imagen = sys.argv[2]
         DPI = int(sys.argv[3])
-        Ancho_Linea = float(sys.argv[4])
-        
+        Ancho_Linea = int(sys.argv[4])
+
         # Leer el archivo DXF
         doc = ezdxf.readfile(ruta_dxf)
 
@@ -21,18 +21,26 @@ def DXF_convet_PNG():
         msp = doc.modelspace()
 
         for entidad in msp:
+            if entidad.dxf.linetype=='Continuous':
+                Color='BLACK'
+            else:
+                Color='GREEN'
+
             if entidad.dxftype() == 'LINE':
                 # Dibujar una línea desde el punto inicial al final
-                plt.plot([entidad.dxf.start[0], entidad.dxf.end[0]], [entidad.dxf.start[1], entidad.dxf.end[1]], color='black', antialiased=True, linewidth=Ancho_Linea)
-
+                plt.plot([entidad.dxf.start[0], entidad.dxf.end[0]], [entidad.dxf.start[1], entidad.dxf.end[1]], color=Color, antialiased=True, linewidth=Ancho_Linea)
             elif entidad.dxftype() == 'CIRCLE':
                 # Dibujar un círculo
-                circulo = Circle((entidad.dxf.center[0], entidad.dxf.center[1]), entidad.dxf.radius, fill=False, antialiased=True, linewidth=Ancho_Linea)
+                circulo = Circle((entidad.dxf.center[0], entidad.dxf.center[1]), entidad.dxf.radius, fill=False, color=Color, antialiased=True, linewidth=Ancho_Linea)
                 ax.add_patch(circulo)
             elif entidad.dxftype() == 'ARC':
                 # Dibujar un arco
-                arco = Arc((entidad.dxf.center[0], entidad.dxf.center[1]), 2*entidad.dxf.radius, 2*entidad.dxf.radius, theta1=entidad.dxf.start_angle, theta2=entidad.dxf.end_angle, fill=False, antialiased=True, linewidth=Ancho_Linea)
+                arco = Arc((entidad.dxf.center[0], entidad.dxf.center[1]), 2*entidad.dxf.radius, 2*entidad.dxf.radius, theta1=entidad.dxf.start_angle, theta2=entidad.dxf.end_angle, fill=False, color=Color, antialiased=True, linewidth=Ancho_Linea)
                 ax.add_patch(arco)
+            elif entidad.dxftype() in 'LWPOLYLINE':
+                # Dibujar una polilínea
+                puntos = [punto for punto in entidad]
+                plt.plot(*zip(*puntos), color=Color, antialiased=True, linewidth=Ancho_Linea)
 
         # Ajustar los límites de la gráfica para que incluyan todas las entidades
         ax.axis('equal')
